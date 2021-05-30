b7a7494baa773d422365da17a9c65a1a0057d6d9
MariaLink24 <fromnarniawithlove@gmail.com>
2021-05-30 13:47:09 +0300

    add 1st ver of program + testfile

---
+++ regexpLab/src/ru/miet/smirnovammpin31d/Main.java
@@ -0,0 +1,27 @@
+package ru.miet.smirnovammpin31d;
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+    public static void main(String[] args) {
+        try
+        {
+            Path file = Paths.get("src/ru/miet/smirnovammpin31d/test.txt");
+            BufferedReader reader = new BufferedReader(new FileReader(String.valueOf(file)));
+            String line = "", oldtext = "";
+            while((line = reader.readLine()) != null)
+            {
+                oldtext += line + "\r\n";
+            }
+            reader.close();
+            String newtext = oldtext.replaceAll("drink", "[censored]");
+            FileWriter writer = new FileWriter("file.txt");
+            writer.write(newtext);writer.close();
+        }
+        catch (IOException ioe)
+        {
+            ioe.printStackTrace();
+        }
+    }
+}
