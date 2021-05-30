5e8b6115430135f5210881a421baeb4d1ea679a4
MariaLink24 <fromnarniawithlove@gmail.com>
2021-05-30 15:35:10 +0300

    add test

--- gradleLab/src/test/java/StopwatchTest.java
+++ gradleLab/src/test/java/StopwatchTest.java
@@ -1,8 +1,12 @@
 import org.junit.Test;
+import org.testng.Assert;
 
 public class StopwatchTest {
     @Test
     public void testStart() {
-
+    Stopwatch stopwatch = new Stopwatch();
+    stopwatch.start();
+    stopwatch.stop();
+    Assert.assertEquals(stopwatch.toString(), 1643);
     }
 }
